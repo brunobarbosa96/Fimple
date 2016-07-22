f4e4c8e901b507e7797ce4a0b8a343cd4933576c
Bruno Barbosa <brunobarbosa96@hotmail.com>
2016-07-21 22:08:39 -0300

    Commit - Exemplo web com requisição API NODE

---
+++ Web/Exemplo/Exemplo/Exemplo/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Optimization;
+
+namespace Exemplo
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bower/jquery").Include(
+                      "~/bower_components/jquery/dist/jquery.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ Web/Exemplo/Exemplo/Exemplo/ViewModels/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exemplo.ViewModels
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birth { get; set; }
+
+        public string DataNascimento => Birth.ToShortDateString();
+    }
+}
