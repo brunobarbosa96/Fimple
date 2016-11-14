98d056af392b7415eb4605156b63a2686c78324e
Bruno Barbosa <brunobarbosa96@hotmail.com>
2016-11-14 21:43:49 -0200

    cripty development

--- Web/FimpleWeb/Home/Controllers/Usuario/UsuarioController.cs
+++ Web/FimpleWeb/Home/Controllers/Usuario/UsuarioController.cs
@@ -1,6 +1,7 @@
 ﻿using Home.Application.Curso;
 using Home.Application.Usuario;
 using Home.Infra;
+using Home.Infra.Security;
 using Home.Models.Entity;
 using Newtonsoft.Json;
 using System;
@@ -45,11 +46,8 @@ namespace Home.Controllers.Usuario
         {
             try
             {
-                //if (!ModelState.IsValid)
-                //{
-                //    ValidateModel(usuario);
-                //    return RedirectToAction("GetDados", "Usuario", usuario);
-                //}
+                // Criptografando senha
+                usuario.Senha = Security.Encrypt(usuario.Senha);
 
                 // Requisição para inserir usuário
                 var response = _usuarioApp.Post(usuario);
