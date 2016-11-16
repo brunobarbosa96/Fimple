365eed0084389d1819c10c8c0e1adf22abfc447a
Bruno Barbosa <brunobarbosa96@hotmail.com>
2016-11-16 21:21:08 -0200

    fix bug publish configuration

--- Web/FimpleWeb/Home/Infra/Config/UriWebApi.cs
+++ Web/FimpleWeb/Home/Infra/Config/UriWebApi.cs
@@ -1,14 +1,13 @@
-﻿using System.Diagnostics;
-
-namespace Home.Infra.Config
+﻿namespace Home.Infra.Config
 {
     public static class UriWebApi
     {
-        private static string Server => Debugger.IsAttached ? "http://localhost" : "https://fimple.herokuapp.com/";
-        public static string Login => $"{Server}:21066/api/authentication/Login/";
-        public static string Usuario => $"{Server}:21066/api/usuario/";
-        public static string Curso => $"{Server}:21066/api/curso/";
-        public static string Chat => $"{Server}:21066/api/chat/";
-        public static string Publicacao => $"{Server}:21066/api/timeline/publicacao/";
+        //private static string Server => Debugger.IsAttached ? "http://localhost:21066/" : "https://fimple.herokuapp.com/";
+        private static string Server => "https://fimple.herokuapp.com/";
+        public static string Login => $"{Server}api/authentication/Login/";
+        public static string Usuario => $"{Server}api/usuario/";
+        public static string Curso => $"{Server}api/curso/";
+        public static string Chat => $"{Server}api/chat/";
+        public static string Publicacao => $"{Server}api/timeline/publicacao/";
     }
 }
