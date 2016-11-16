3070ab57d69ce69fab5ef2ecf2d5127064617680
JorgeBarbosaS <jorangerbarbosa@gmail.com>
2016-11-15 22:49:16 -0200

    Tela Mapa

---
+++ Web/FimpleWeb/Home/Controllers/Mapa/MapaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+using Home.Infra;
+using Home.Infra.Security;
+using Newtonsoft.Json;
+
+namespace Home.Controllers.Mapa
+{
+    public class MapaController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
